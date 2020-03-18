84f130df0c5e2c81864c22b6cdb27da135162fd2
pkro <pkrombach@web.de>
2020-03-18 08:16:54 +0100

    db connection & basic api routes

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config'); // found because of config module installed
+const db = config.get('mongoURI');
+console.log(db);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true, // these 2 options because of deprecation warning
+      useNewUrlParser: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.err(err.message);
+    process.exit(1); // exit with failure
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const authRoute = require('./routes/api/auth');
 
 const app = express();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('server running');
 });
+
+//Define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
