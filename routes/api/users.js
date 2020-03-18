ce05f4dd09c94bf602e192dcf0ee96907d5223b3
pkro <pkrombach@web.de>
2020-03-18 11:50:28 +0100

    started fleshing out user auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,40 @@
 const express = require('express');
-
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name required')
+      .not()
+      .isEmpty(),
+    check('email', 'Email missing or wrong format').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+    console.log(req.body);
+    res.send('User route');
+  }
+
+  // user exists?
+
+  // get user gravatar
+
+  // encrypt pass
+
+  // return jsonwebtoken
+);
 
 module.exports = router;
+
+//check('email', 'Email missing or wrong format', isEmail());
