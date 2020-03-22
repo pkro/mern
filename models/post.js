09aa312def50961248f38a9440f11cb93a9ed4aa
pkro <pkrombach@web.de>
2020-03-22 07:45:59 +0100

    added post model

---
+++ models/post.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    // username so posts can be kept
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    // so likes can be given only once
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: { type: String },
+      avatar: { type: String },
+      date: { type: Date, default: Date.now },
+    },
+  ],
+
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model(postSchema);
