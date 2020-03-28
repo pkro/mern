65291242b663318bde5b14661394870163ad4a0d
pkro <pkrombach@web.de>
2020-03-28 15:53:41 +0100

    Lin to instead of a href

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -74,7 +75,7 @@ const Register = () => {
         <input type="submit" value="register" className="btn btn-primary" />
       </form>
       <p className="my-1">
-        Already registered? <a href="login.html">Sign in</a>
+        Already registered? <Link to="/login">Sign in</Link>
       </p>
     </Fragment>
   );
