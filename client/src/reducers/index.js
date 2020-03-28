d7c0393c426dc840bd856b2559af438a3f5a695b
pkro <pkrombach@web.de>
2020-03-28 19:45:14 +0100

    added alert reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,4 @@
+// this file is the root reducer
 import { combineReducers } from 'redux';
-
-export default combineReducers({});
+import alert from './alert';
+export default combineReducers({ alert });
