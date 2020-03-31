70d8b1240f437937ff18c695dfb484eb6f6ec11e
pkro <pkrombach@web.de>
2020-03-31 13:14:02 +0200

    moved condition to jsx from link var definition

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,39 +4,31 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Navbar = ({ isAuthenticated, logout }) => {
-  let links;
-  if (isAuthenticated) {
-    links = (
-      <ul>
-        <li>
-          <Link to="/profiles">Developers</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/" onClick={logout}>
-            Logout
-          </Link>
-        </li>
-      </ul>
-    );
-  } else {
-    links = (
-      <ul>
-        <li>
-          <Link to="/profiles">Developers</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
-    );
-  }
+const Navbar = ({ isAuthenticated, loading, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to="/" onClick={logout}>
+          <i className="fas fa-sign-out-alt"></i>
+          <span className="hide-sm">Logout</span>
+        </Link>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/profiles">Developers</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  );
 
   return (
     <nav className="navbar bg-dark">
@@ -46,17 +38,20 @@ const Navbar = ({ isAuthenticated, logout }) => {
         </Link>
         Dev connector
       </h1>
-      {links}
+      {!loading && (isAuthenticated ? authLinks : guestLinks)}
     </nav>
   );
 };
 
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated,
+  loading: state.auth.loading,
 });
 
 Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default connect(mapStateToProps, { logout })(Navbar);
