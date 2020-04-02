44a9087e507dfb3da89c3b2fddddad877dd23d9e
pkro <pkrombach@web.de>
2020-04-02 14:56:21 +0200

    read in profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 import PropTypes from 'prop-types';
 
-const Dashboard = () => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+  console.log(profile);
   return (
     <div>
       <h1>Dashboard</h1>
     </div>
   );
 };
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-export default Dashboard;
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
