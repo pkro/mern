9edb15b87b1369bc1bd1f8b592f116e6acc4ec47
pkro <pkrombach@web.de>
2020-04-03 13:02:04 +0200

    fixed error return json

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -46,7 +46,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const {
@@ -104,7 +104,6 @@ router.post(
       await profile.save();
       return res.json(profile);
     } catch (err) {
-      console.log(err);
       res.status(500).send('server error');
     }
   }
