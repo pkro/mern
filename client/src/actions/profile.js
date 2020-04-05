ea99992d7d6faee27dc2551db540611f87298e11
pkro <pkrombach@web.de>
2020-04-05 18:47:19 +0200

    extracted redundant config object, added addProfileData

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from '../actions/types';
 import Alert from '../components/layout/Alert';
+
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
 
 // get current users profile
 export const getCurrentProfile = () => async (dispatch) => {
@@ -23,23 +29,39 @@ export const getCurrentProfile = () => async (dispatch) => {
 export const editProfile = (formData, history, edit = false) => async (
   dispatch
 ) => {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
   try {
     const res = await axios.post('/api/profile', formData, config);
     dispatch({ type: GET_PROFILE, payload: res.data }); // as it returns the profile
     dispatch(setAlert(edit ? 'Profile updated' : 'Profile created', 'success'));
-    if (!edit) {
       // we can't use Redirect from react router in an action
       history.push('/dashboard');
-    }
+    
   } catch (err) {
     const errors = err.response.data.errors;
-    console.log(err);
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger', 3000)));
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  }
+};
+
+// add education or experience
+export const addProfileData = (type, formData, history) => async (dispatch) => {
+  if (!['education', 'experience'].includes(type)) {
+    throw 'type parameter must be either "education" or "experience';
+  }
+  try {
+    const res = await axios.put('/api/profile/' + type, formData, config);
+    dispatch({ type: UPDATE_PROFILE, payload: res.data });
+    dispatch(
+      setAlert(type[0].toUpperCase() + type.slice(1) + ' added', 'success')
+    );
+    history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
     if (errors) {
       errors.forEach((error) => dispatch(setAlert(error.msg, 'danger', 3000)));
       dispatch({
