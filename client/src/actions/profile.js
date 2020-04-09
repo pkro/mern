b55261bad252d4d74e793315813021132c5b1c0b
pkro <pkrombach@web.de>
2020-04-09 08:21:30 +0200

    typo in route path

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -35,7 +35,7 @@ export const getCurrentProfile = () => async (dispatch) => {
 // get profile by id
 export const getProfileById = (userId) => async (dispatch) => {
   try {
-    const res = await axios.get(`/api/profile/${userId}`);
+    const res = await axios.get(`/api/profile/user/${userId}`);
     dispatch({
       type: GET_PROFILE,
       payload: res.data,
