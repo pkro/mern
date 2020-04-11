b6beb199cdf34f07d5deb9a920b68b9b2f139dce
pkro <pkrombach@web.de>
2020-04-11 20:05:16 +0200

    show github repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({
   getProfileById,
@@ -26,6 +27,7 @@ const Profile = ({
         <Spinner />
       ) : (
         <Fragment>
+          <div>{profile.githubusername}</div>
           <Link to="/profiles" className="btn">
             Back to profiles
           </Link>
@@ -69,6 +71,9 @@ const Profile = ({
                 <h4>No education credentials</h4>
               )}
             </div>
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
