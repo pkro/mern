86e5d959288128b47d244448dc0cfe1c1c43f54f
pkro <pkrombach@web.de>
2020-04-12 16:13:39 +0200

    like, unlike, delete post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
 
 export const getPostsById = (user) => async (dispatch) => {
   try {
@@ -26,7 +26,33 @@ export const getPosts = () => async (dispatch) => {
   }
 };
 
-export const deletePost = (post) => async (dispatch) => {
+export const addLikesUnlikes = (postId, type) => async (dispatch) => {
   try {
-  } catch (err) {}
+    const res = await axios.put(`/api/posts/${type}/${postId}`);
+    return dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await axios.delete(`/api/posts/${id}`);
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+    dispatch(setAlert('Post removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
 };
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import moment from 'moment';
 import { connect } from 'react-redux';
+import { addLikesUnlikes, deletePost } from '../../actions/post';
 
 // I always forget that props is the top level object, not the individual prop ("post")
 const PostItem = ({
   auth,
-  post: { _id, user, text, likes, comments, date },
+  post: { _id, user, avatar, text, likes, comments, date },
+  addLikesUnlikes,
+  deletePost,
 }) => {
   return (
-    <div className="post bg-white my-1">
+    <div className="post bg-white my-1 p-1">
       <div>
         <Link to={`/profile/${user._id}`}>
-          <img className="round-img" src={user.avatar} alt="profile" />
+          <img className="round-img" src={avatar} alt="profile" />
           <h4>{user.name}</h4>
         </Link>
       </div>
@@ -23,12 +26,12 @@ const PostItem = ({
         <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{moment.utc(date)}</Moment>
         </p>
-        <button className="btn">
+        <button className="btn" onClick={() => addLikesUnlikes(_id, 'like')}>
           <i className="fas fa-thumbs-up"></i>
           {likes.length > 0 && <span> {likes.length}</span>}
         </button>
 
-        <button className="btn">
+        <button className="btn" onClick={() => addLikesUnlikes(_id, 'unlike')}>
           <i className="fas fa-thumbs-down"></i>
         </button>
         <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -37,8 +40,12 @@ const PostItem = ({
             <span className="comment-count">{comments.length}</span>
           )}
         </Link>
-        {!auth.loading && user._id === auth.user._id && (
-          <button className="btn btn-danger" type="button">
+        {!auth.loading && user === auth.user._id && (
+          <button
+            className="btn btn-danger"
+            type="button"
+            onClick={() => deletePost(_id)}
+          >
             <i className="fas fa-times"></i> Delete
           </button>
         )}
@@ -50,8 +57,12 @@ const PostItem = ({
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  addLikesUnlikes: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({ auth: state.auth });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLikesUnlikes, deletePost })(
+  PostItem
+);
