763672e54e2b45e414a499293dda78f2439d72fd
pkro <pkrombach@web.de>
2020-04-13 16:41:09 +0200

    added default timeout

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // TODO: either scroll to top so user can see alert if scrolled down or position alert float on top of screen in css
-export const setAlert = (msg, alertType, msToTimeout) => dispatch => {
+export const setAlert = (msg, alertType, msToTimeout = 3000) => (dispatch) => {
   const id = uuidv4();
   dispatch({
     type: SET_ALERT,
