6af3257fbdf9324836d587a1c4a2dae3d01e2046
pkro <pkrombach@web.de>
2020-04-13 18:22:08 +0200

    removed switch, thus fixing not found on landing bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,32 @@
-import React, { Fragment } from 'react'; // Fragment doesn''t show up in DOM, just necessary to enclose jsx if not using a div or other element
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { useEffect } from 'react';
-import './App.css';
-
-import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
-import Alert from './components/layout/Alert';
-
-import setAuthToken from './utils/setAuthToken';
-// redux
-import store from './store';
-import { Provider } from 'react-redux';
-import { loadUser } from './actions/auth';
-import getStorageProvider from './utils/getStorageProvider';
+import Landing from './components/layout/Landing';
 import Routes from './components/routing/Routes';
 
-const storage = getStorageProvider();
-setAuthToken(storage.token);
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
 
 const App = () => {
   useEffect(() => {
-    // same as localStorage.getItem('token')
+    setAuthToken(localStorage.token);
     store.dispatch(loadUser());
-  }, []); //[] = run only once
+  }, []);
 
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route component={Routes} />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
